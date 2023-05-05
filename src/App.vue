076d56058cc3cd5811b93747644653dfdf6d0a30
Filipe Santos <filipssant@gmail.com>
2023-05-05 17:43:09 -0300

    add task tracker logo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="columns is-gapeless is-multiline">
     <div class="column is-one-quarter">
-
+      <BarraLateral/>
     </div>
     <div>
       
@@ -11,9 +11,11 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import BarraLateral from './components/BarraLateral.vue'
 export default defineComponent({
   name: 'App',
   components: {
+    BarraLateral
   }
 });
 </script>
--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
-
-  </div>
+<header>
+  <h1>
+    <img id="sidebar-logo" src="@/assets/task-tracker-logo.svg" alt="" class="sideBar__logo">
+  </h1>
+</header>
 </template>
 
 <script lang="ts">
@@ -9,12 +11,14 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({
   setup () {
-  
-      return {}
+    return {}
   }
 })
 </script>
 
 <style scoped>
-
+  .sideBar__logo{
+    width: 200px;
+    height: auto;
+  }
 </style>
