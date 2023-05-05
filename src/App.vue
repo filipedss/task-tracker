5923993beb5b19d14a5c8b48f53affd1e3950ed6
Filipe Santos <filipssant@gmail.com>
2023-05-05 16:43:24 -0300

    vbase 3

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
+  <main class="columns is-gapeless is-multiline">
+    <div class="column is-one-quarter">
 
+    </div>
+    <div>
+      
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-
 export default defineComponent({
   name: 'App',
   components: {
