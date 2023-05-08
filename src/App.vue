44cd829e5e3eb6bafd270eeb6fded72226b19ec3
Filipe Santos <filipssant@gmail.com>
2023-05-08 15:14:29 -0300

    import trackForm on app

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
     <div class="column is-one-quarter">
       <BarraLateral/>
     </div>
-    <div>
-      
+    <div class="column is-three-quarter">
+      <TrackForm/>
     </div>
   </main>
 </template>
@@ -12,10 +12,12 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import BarraLateral from './components/BarraLateral.vue'
+import TrackForm from './components/TrackForm.vue'
 export default defineComponent({
   name: 'App',
   components: {
-    BarraLateral
+    BarraLateral,
+    TrackForm
   }
 });
 </script>
