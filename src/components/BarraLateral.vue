77185363f1fc1087a7ad22b85539a225deb88267
Filipe Santos <filipssant@gmail.com>
2023-05-08 14:17:19 -0300

    logo padding adjust

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -1,5 +1,5 @@
 <template>
-<header>
+<header class="sideBar__header">
   <h1>
     <img id="sidebar-logo" src="@/assets/task-tracker-logo.svg" alt="" class="sideBar__logo">
   </h1>
@@ -20,5 +20,13 @@ export default defineComponent({
   .sideBar__logo{
     width: 200px;
     height: auto;
+  }
+
+  .sideBar__header{
+    background-color: #f1f1f1;
+    height: 100vh;
+    width: 100%;
+    box-shadow: -1px 0px 6px rgba(0,0,0,.5);
+    padding: 1rem 0 0 1rem;
   }
 </style>
